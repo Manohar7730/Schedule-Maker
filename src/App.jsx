d918c531044bf04bcf020d0fbaf3736c3e5c84bb
Manohar <manohar20century@gmail.com>
2026-02-03 20:43:40 +0530

    added form with controlled input

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "./components/Form";
 
 export default function App() {
-  return <div>App</div>;
+  const [schedule, setSchedule] = useState({
+    title: "",
+    description: "",
+    checked: false,
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSchedule((prevSchedule) => ({
+      ...prevSchedule,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <Form schedule={schedule} handleChange={handleChange} />
+    </div>
+  );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Form({ schedule, handleChange }) {
+  return (
+    <div>
+      <form>
+        <label htmlFor="schedule">Title: </label>
+        <input
+          id="schedule"
+          type="text"
+          name="title"
+          value={schedule.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="description">Description: </label>
+        <input
+          id="description"
+          type="text"
+          name="description"
+          value={schedule.description}
+          onChange={handleChange}
+        />
+        <button type="submit"> Add to Schedule </button>
+      </form>
+    </div>
+  );
+}
